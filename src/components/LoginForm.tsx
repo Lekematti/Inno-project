c0558f5bdeed25bfb284d0f00ee03185006e99ad
JerbsH <hippelainen.jere@gmail.com>
2025-03-30 15:43:33 +0300

    Add base files for auth and fix paths

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../app/context/AuthContext'
 import Link from 'next/link'
 
 export default function LoginForm() {

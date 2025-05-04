60e4e8a75f7e970e4b528713c2d5bd3a14ec0e73
JerbsH <hippelainen.jere@gmail.com>
2025-05-04 17:12:33 +0300

    Modify header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,81 +1,140 @@
 import Link from 'next/link'
+import { useState } from 'react'
+import styles from './Header.module.css'
 
 export const Header = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true)
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed)
+
   return (
-    <div className="w-100 p-2 d-flex bg-primary align-items-center">
-      <p className="text-dark fw-bold mb-0">AiWebsiteBuildr</p>
-      <ul className="d-flex w-100 flex-row justify-content-end list-unstyled m-1">
-        <Link
-          className="fs-6 p-1 mx-1 text-dark d-flex flex-column align-items-center text-decoration-none "
-          href={'/'}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="bi bi-house-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
-            <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
-          </svg>
-          HOME
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
+      <div className="container">
+        <Link href="/" className="navbar-brand fw-bold text-primary">
+          AiWebsiteBuildr
         </Link>
 
-        <Link
-          className="fs-6 p-1 mx-1 text-dark d-flex flex-column align-items-center text-decoration-none"
-          href={'/build'}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="bi bi-tools"
-            viewBox="0 0 16 16"
-          >
-            <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z" />
-          </svg>
-          BUILD
-        </Link>
-        <Link
-          className="fs-6 p-1 mx-1 text-dark d-flex flex-column align-items-center text-decoration-none"
-          href={'/contact'}
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}
+          id="navbarNav"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="bi bi-envelope-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
-          </svg>
-          CONTACT
-        </Link>
-        <Link
-          className="p-1 mx-1 text-dark d-flex flex-column align-items-center text-decoration-none"
-          href={'/profile'}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-            />
-          </svg>
-          PROFILE
-        </Link>
-      </ul>
-    </div>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item mx-1">
+              <Link
+                href="/"
+                className="nav-link d-flex flex-column align-items-center"
+                style={{
+                  transition: 'color 0.3s ease',
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-house-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+                  <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+                </svg>
+                <span className="small">HOME</span>
+              </Link>
+            </li>
+            <li className="nav-item mx-1">
+              <Link
+                href="/build"
+                className="nav-link d-flex flex-column align-items-center"
+                style={{
+                  transition: 'color 0.3s ease',
+                  ':hover': { color: '#0d6efd', fill: '#0d6efd' },
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-tools"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z" />
+                </svg>
+                <span className="small">BUILD</span>
+              </Link>
+            </li>
+            <li className="nav-item mx-1">
+              <Link
+                href="/contact"
+                className="nav-link d-flex flex-column align-items-center"
+                style={{
+                  transition: 'color 0.3s ease',
+                  ':hover': { color: '#0d6efd', fill: '#0d6efd' },
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-envelope-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+                </svg>
+                <span className="small">CONTACT</span>
+              </Link>
+            </li>
+            <li className="nav-item mx-1">
+              <Link
+                href="/profile"
+                className="nav-link d-flex flex-column align-items-center"
+                style={{
+                  transition: 'color 0.3s ease',
+                  ':hover': { color: '#0d6efd', fill: '#0d6efd' },
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-person-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path
+                    fillRule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                  />
+                </svg>
+                <span className="small">PROFILE</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <style jsx global>{`
+        .navbar .nav-link:hover {
+          color: #0d6efd !important;
+        }
+        .navbar .nav-link:hover svg {
+          fill: #0d6efd !important;
+        }
+      `}</style>
+    </nav>
   )
 }
